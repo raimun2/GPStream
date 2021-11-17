d329868484bce10422e5b18db401cfce5bc1d78c
dr R <raimun2@users.noreply.github.com>
2021-11-17 19:47:28 -0300

    fix read bugs

--- R/read_stream_file.R
+++ R/read_stream_file.R
@@ -21,12 +21,44 @@ read_stream_file <- function(filename){
     stream_df$timestamp <- lubridate::as_datetime(stream_df$timestamp) #parse datetime
 
   } else if(grep(".gpx$",filename)==1) { # if gpx file use plotKML library
-    file_data <- plotKML::readGPX(filename)
-    stream_df <- tibble::as_tibble(file_data$tracks[[1]][[1]])
-    stream_df$ele <- as.numeric(stream_df$ele) #parse elevation as numeric
-    stream_df$time <- lubridate::as_datetime(stream_df$time) #parse datetime
+    file_data <- read_GPX(filename)
   } else{
     message("unsupported format")
   }
   return(stream_df) #return tibble with file stream
 }
+
+
+#' Read GPX file into stream
+#' more flexible than plotKML function
+#'
+#' @param filename The file path to the containing file
+#' read_GPX()
+#'
+#' @return
+#' @export
+#'
+read_GPX <- function(filename){
+  pfile <- XML::htmlTreeParse(file = filename,
+                             error = function(...) { },
+                             useInternalNodes = T)
+
+  eles <-    as.numeric(XML::xpathSApply(pfile, path = "//trkpt/ele|//rtept/ele", XML::xmlValue))
+  times <-   XML::xpathSApply(pfile, path = "//trkpt/time|//rtept/time", XML::xmlValue)
+  coords <- XML::xpathSApply(pfile, path = "//trkpt|//rtept", XML::xmlAttrs)
+  lats <- as.numeric(coords["lat",])
+  lons <- as.numeric(coords["lon",])
+
+  stream <- data.frame(lat = lats, lon = lons)
+
+  if(length(eles) == length(lats)){
+    stream$ele = eles
+  }
+  if(length(times) == length(lats)){
+    stream$time = lubridate::as_datetime(times)
+  }
+
+  return(stream)
+}
+
+
